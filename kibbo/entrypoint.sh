44f8a39b054aa1fbaa00239d0f3fd5c1133df6fb
Ahmad Musleh <ahmad.s.musleh@gmail.com>
2023-10-13 23:09:39 +0300

    Replace file_redirect env variable with labels for file redirects

--- kibbo/entrypoint.sh
+++ kibbo/entrypoint.sh
@@ -28,15 +28,28 @@ check_if_logger_running() {
     fi
 }
 
+set_file_redirect_operator() {
+    local append
+    append=$(eval "docker container inspect $hostname | jq -r \".[0].Config.Labels.\\\"kibbo.config.log_file_update_mode\\\"\"")
+
+    case $append in
+        "append")
+            echo "Log file redirect mode: append"
+            file_redirect_operator=">>"
+            ;;
+        "replace")
+            echo "Log file redirect mode: replace"
+            file_redirect_operator=">"
+            ;;
+        *)
+            echo "Invalid redirect mode: $append. Defaulting to replace"
+            file_redirect_operator=">"
+    esac
+}
+
 run_logs_for_container() {
     local container_id=$1
     local container_name=$2
-
-    if [ "$APPEND" = "TRUE" ]; then
-        local append=">>"
-    else
-        local append=">"
-    fi
 
     if [ "$INCLUDE_TIMESTAMPS" = "TRUE" ]; then
         local include_timestamps="--timestamps"
@@ -48,12 +61,25 @@ run_logs_for_container() {
 
     etcdctl put "$container_id" "$container_name"
 
-    command="docker logs -f $include_timestamps $container_id $append /logs/$container_name.log 2>&1"
+    command="docker logs -f $include_timestamps $container_id $file_redirect_operator /logs/$container_name.log 2>&1"
     eval "$command"
 
     echo "Removing $container_name from etcd"
     etcdctl del $container_id
+}
 
+set_opt_in_out() {
+    opt_setting=$(eval "docker container inspect $hostname | jq -r \".[0].Config.Labels.\\\"kibbo.config.log_mode\\\"\"")
+    case $opt_setting in
+        "optout")
+            echo "Services: Opt out"
+            ;;
+        "replace")
+            echo "Services: Opt in"
+            ;;
+        *)
+            echo "Invalid opt mode: $opt_setting. Defaulting to Opt out"
+    esac
 }
 
 
@@ -70,7 +96,7 @@ check_and_update_loggers() {
                 if [ $logger_running -eq "0" ]; then
                     echo "$container_name logger not started. Starting..."
                     run_logs_for_container "$container_id" "$container_name" &
-                    
+
                 else
                     echo "$container_name logger running already. Skipping..."
                 fi
@@ -79,16 +105,29 @@ check_and_update_loggers() {
     return
 }
 
-setup_etcd
+set_hostname() {
+    hostname=$(cat /etc/hostname)
+    etcdctl put hostname $hostname
+    echo "Hostname: $hostname"
+}
 
-hostname=$(cat /etc/hostname)
-etcdctl put hostname $hostname
-echo "hostname is $hostname"
-network=$(docker ps --format json | jq -r 'select(.ID=="'$hostname'") | .Networks')
-etcdctl put network $network
-echo "Network is $network"
+set_network() {
+    network=$(docker ps --format json | jq -r 'select(.ID=="'$hostname'") | .Networks')
+    etcdctl put network $network
+    echo "Network: $network"
+}
 
-while true; do
-    check_and_update_loggers;
-    sleep 10; # TODO: wait for all background processes to exit instead
-done
+main() {
+    setup_etcd
+    set_hostname
+    set_network
+    set_opt_in_out
+    set_file_redirect_operator
+
+    while true; do
+        check_and_update_loggers;
+        sleep 10; # TODO: wait for all background processes to exit instead
+    done
+}
+
+main
