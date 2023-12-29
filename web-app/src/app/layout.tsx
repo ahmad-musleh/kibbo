bad3f6f8276dd35487c268b40d259934a5a92c39
Ahmad Musleh <ahmad.s.musleh@gmail.com>
2023-12-29 14:44:24 +0300

    web-app: Setup coming soon using next js

---
+++ web-app/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Kibbo',
+  description: 'Docker logs thrown where you want them.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
