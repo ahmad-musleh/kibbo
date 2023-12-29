2b6515d9147599e6c458abf1cd47ef8d6f395586
Ahmad Musleh <ahmad.s.musleh@gmail.com>
2023-12-29 14:52:02 +0300

    web-app: Build kibbo skeleton

---
+++ web-app/src/app/footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return(
+        <>
+            <div>
+                <footer>
+                    Kibbo by Musleh
+                </footer>
+            </div>
+        </>
+    )
+};
