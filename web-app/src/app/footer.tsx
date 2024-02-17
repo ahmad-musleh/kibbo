c94e66e43a350c20e7164c384bd6bb890bdd8896
Ahmad Musleh <ahmad.s.musleh@gmail.com>
2024-02-17 20:58:22 +0300

    web-app: Update footer

--- web-app/src/app/footer.tsx
+++ web-app/src/app/footer.tsx
@@ -1,10 +1,16 @@
+import Image from 'next/image';
 export default function Footer() {
     return(
         <>
-            <div>
-                <footer>
-                    Kibbo by Musleh
-                </footer>
+            <div className='flex justify-center items-center'>
+                <a href='https://github.com/ahmad-musleh/kibbo'>
+                    <Image
+                    src='/kibbo.svg'
+                    width={300}
+                    height={300}
+                    alt='Kibbo logo'
+                    />
+                </a>
             </div>
         </>
     )
