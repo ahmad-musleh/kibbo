d7329dad4182bae631a82da0918484660951c75d
Ahmad Musleh <ahmad.s.musleh@gmail.com>
2025-02-02 19:14:40 +0300

    web-app: adjust logo size and text alignment

--- web-app/src/app/footer.tsx
+++ web-app/src/app/footer.tsx
@@ -5,10 +5,10 @@ export default function Footer() {
       <div className="grid grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1">
         <div className="place-self-center pt-10 lg:place-self-end">
           <a href="https://github.com/ahmad-musleh/kibbo">
-            <Image src="/kibbo.svg" width={300} height={300} alt="Kibbo logo" />
+            <Image src="/kibbo.svg" width={250} height={250} alt="Kibbo logo" />
           </a>
         </div>
-        <div className="m-4 h-full max-w-xl place-self-center p-4 text-center lg:m-10 lg:place-self-start lg:p-10">
+        <div className="m-4 h-full max-w-xl place-self-center p-4 text-center lg:m-10 lg:ml-4 lg:place-self-start lg:p-10 lg:pl-0 lg:text-left">
           <span className="text-lg text-gray-400">
             Kibbo streamlines your Docker workflow by eliminating the hassle of
             manual commands and file outputs. Easily log different services to
